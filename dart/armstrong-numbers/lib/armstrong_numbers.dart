2aca38c42b08f3e446ce5f6f363b47536723fb2a
VinuBalagopalAP <vinubalagopalap@gmail.com>
2023-01-03 01:57:43 +0530

    ArmstrongNumbers - 2nd iteration

--- dart/armstrong-numbers/lib/armstrong_numbers.dart
+++ dart/armstrong-numbers/lib/armstrong_numbers.dart
@@ -1,15 +1,10 @@
 class ArmstrongNumbers {
   // Put your code here
   bool isArmstrongNumber(String numString) {
-    int num = int.parse(numString);
-    int numLength = numString.length;
-    int sum = 0;
-
-    for (int i = 0; i < numLength; i++) {
-      int digit = int.parse(numString[i]);
-      sum += digit * digit * digit;
-    }
-
-    return sum == num;
+    return int.parse(numString) ==
+        numString.split('').fold(
+            0,
+            (sum, digit) =>
+                sum + int.parse(digit) * int.parse(digit) * int.parse(digit));
   }
 }
