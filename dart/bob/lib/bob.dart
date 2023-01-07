52682cf82d5a293dcdaecf2d41f9a3f6a0c43490
VinuBalagopalAP <vinubalagopalap@gmail.com>
2023-01-08 02:01:13 +0530

    Bob

--- dart/bob/lib/bob.dart
+++ dart/bob/lib/bob.dart
@@ -1,8 +1,7 @@
 class Bob {
   // Put your code here
   String response(String input) {
-    String string = input.replaceAll(r"\'", "");
-    if (string.trim().isEmpty) {
+    if (input.trim().isEmpty) {
       return 'Fine. Be that way!';
     } else if (input.toUpperCase() == input && input.toLowerCase() != input) {
       return 'Whoa, chill out!';
