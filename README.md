ca0299256b069d2158d22f22fdcfbb236b1f7abc
VinuBalagopalAP <vinubalagopalap@gmail.com>
2023-01-08 00:45:41 +0530

    Difference between sqaures

--- README.md
+++ README.md
@@ -8,3 +8,5 @@
 3. [Leap](dart/leap/lib/leap.dart)
 4. [Scrabble Score](dart/scrabble-score/lib/scrabble_score.dart)
 5. [Armstrong Numbers](dart/armstrong-numbers/lib/armstrong_numbers.dart)
+6. [High Scores](dart/high-scores/lib/high_scores.dart)
+7. [Difference of Squares](dart/difference-of-squares/lib/difference_of_squares.dart)
--- dart/difference-of-squares/lib/difference_of_squares.dart
+++ dart/difference-of-squares/lib/difference_of_squares.dart
@@ -1,13 +1,19 @@
 class DifferenceOfSquares {
   int squareOfSum(int n) {
-    return (n * (n + 1) / 2) * (n * (n + 1) / 2) as int;
+    ///  [ Calculate the sum of the first n natural numbers ]
+    int sum = (n * (n + 1)) ~/ 2;
+
+    /// [ Return the square of the sum ]
+    return sum * sum;
   }
 
   int sumOfSquares(int n) {
-    return n * (n + 1) * (2 * n + 1) / 6 as int;
+    /// [ Return the sum of the squares of the first n natural numbers ]
+    return (n * (n + 1) * (2 * n + 1)) ~/ 6;
   }
 
   int differenceOfSquares(int n) {
+    /// [ Return the difference between the square of the sum and the sum of the squares ]
     return squareOfSum(n) - sumOfSquares(n);
   }
 }
